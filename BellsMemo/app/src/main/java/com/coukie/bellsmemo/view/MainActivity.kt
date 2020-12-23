21ac3162e043bab89381e3029d1be159f8cfb052
blueboy1593 <snb0303@naver.com>
2020-12-24 02:18:21 +0900

    Search Bar Done

--- BellsMemo/app/src/main/java/com/coukie/bellsmemo/view/MainActivity.kt
+++ BellsMemo/app/src/main/java/com/coukie/bellsmemo/view/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.coukie.bellsmemo.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coukie.bellsmemo.R
+import com.coukie.bellsmemo.model.room.RoomMemo
 import com.coukie.bellsmemo.presenter.MemoHelper
 import com.coukie.bellsmemo.presenter.MemoPresenter
 import kotlinx.android.synthetic.main.activity_main.*
@@ -24,5 +26,23 @@ class MainActivity : AppCompatActivity() {
         noterecyclerView.adapter = adapter
 
         presenter.intentWriteMemoActivity(writeMemo, WriteMemoActivity::class.java)
+
+        searchButton.setOnClickListener {
+            val searchItem = searchText.text
+            val searchData = mutableListOf<RoomMemo>()
+            adapter.listData.forEach {
+                if (it.title.contains(searchItem) or it.content.contains(searchItem)) {
+                    searchData.add(it)
+                }
+            }
+            adapter.listData = searchData
+            noterecyclerView.adapter = adapter
+        }
+
+        goHome.setOnClickListener {
+            finishAffinity()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
